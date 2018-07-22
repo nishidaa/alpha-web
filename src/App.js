0178c3dab2d415e342b06ad63faadec7fd7fdc3a
d24as4r4 <sara.aomori@gmail.com>
2018-07-22 12:47:36 +0900

    commit test

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Alpha開発部隊秘密基地</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        　mkcndisk
         </p>
       </div>
     );
