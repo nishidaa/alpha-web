720901a0f7c65e6729c52e10c744fb351bc37b48
d24as4r4 <sara.aomori@gmail.com>
2018-09-30 14:40:04 +0900

    更新履歴をカードに変更

--- src/components/views/Top.js
+++ src/components/views/Top.js
@@ -10,12 +10,18 @@ import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main:"#e6e6e6"
-       }
+      main: "#e6e6e6"
+    }
   },
 });
 
@@ -23,7 +29,7 @@ const styles = theme => ({
   root: {
     flexGrow: 1,
   },
-  
+
   button: {
     margin: theme.spacing.unit,
   },
@@ -38,8 +44,22 @@ const styles = theme => ({
   },
   largeText: {
     fontSize: '40px'
+  },
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    marginBottom: 16,
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
   }
-  
 });
 
 
@@ -47,48 +67,48 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {count: 0};
+    this.state = { count: 0 };
     this.tick = this.tick.bind(this);
   }
 
   tick() {
-    this.setState({count: this.state.count + 1});
+    this.setState({ count: this.state.count + 1 });
   }
 
-  getThreeTimesStyle(){
-    return this.state.count % 3 == 0 && this.state.count != 0? styles.largeText :styles.normal;
+  getThreeTimesStyle() {
+    return this.state.count % 3 == 0 && this.state.count != 0 ? styles.largeText : styles.normal;
   }
 
   handleToProductsPage = () => {
     this.props.history.push('/products')
   }
-  
+
   handleChange = (value) => {
     this.setState({ value });
   };
-  
+
 
   render() {
     return (
-      <MuiThemeProvider theme = {theme}>
+      <MuiThemeProvider theme={theme}>
         <div className="App">
           <header className="App-header">
             <nav>
               <ul>
                 <li><img src={logo} className="App-logo" alt="logo" /></li>
                 <li>
-                <Paper >
-                  <Tabs
-                    indicatorColor="primary"
-                    textColor="primary"
-                    
-                  >
-                    <Tab label="トップ" />
-                    <Tab label="シリーズ" />
-                    <Tab label="商品検索" />
-                    <Tab label="ショールーム" />
-                  </Tabs>
-                </Paper>
+                  <Paper >
+                    <Tabs
+                      indicatorColor="primary"
+                      textColor="primary"
+
+                    >
+                      <Tab label="トップ" />
+                      <Tab label="シリーズ" />
+                      <Tab label="商品検索" />
+                      <Tab label="ショールーム" />
+                    </Tabs>
+                  </Paper>
 
                 </li>
                 <li>
@@ -105,27 +125,39 @@ class App extends Component {
             <img src={TopShowroom} className="Top-showroom" alt="logo" />
           </div>
           <div classname="TopContents">
-            <li><a className="ContentsText">News</a></li>
-            <li><a className="SubContentsText">Nanika1</a></li>
-            <li><a className="SubContentsText">Nanika2</a></li>
+            <li>
+              <CardContent>
+                <Typography color="textSecondary">
+                  最新情報（更新日）
+                  </Typography>
+                <Typography variant="headline" component="h2">
+                  10月〇日　TOCとくのいち
+                  </Typography>
+                <Typography component="p">
+                  詳細は<button>こちら</button>
+                </Typography>
+              </CardContent>
+
+            </li>
+
 
           </div>
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
           <TextField
-           hintText="あなたのお名前"
-           style={styles.textbox}
-         /><br />
-         <TextField
-           hintText="お問い合わせ内容"
-           style={styles.textbox}
-           multiLine={true}
-           rows={2}
-         /><br />
-         <p style={this.getThreeTimesStyle()}>{this.state.count}回送信しました</p>
+            hintText="あなたのお名前"
+            style={styles.textbox}
+          /><br />
+          <TextField
+            hintText="お問い合わせ内容"
+            style={styles.textbox}
+            multiLine={true}
+            rows={2}
+          /><br />
+          <p style={this.getThreeTimesStyle()}>{this.state.count}回送信しました</p>
         </div>
-      </MuiThemeProvider>
+      </MuiThemeProvider >
     );
   }
 }
